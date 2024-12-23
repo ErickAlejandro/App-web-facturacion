8a0dde0eb8d4263cfd1f336b5ef949cc0697aa73
ErickAlejandro <acea1234@hotmail.com>
2024-12-23 12:17:46 -0500

    Added info in enterprise view

--- webBilling/static/js/enterprise.js
+++ webBilling/static/js/enterprise.js
@@ -1,5 +1,14 @@
 let userData = JSON.parse(document.getElementById('userData').textContent);
 let enterpriseData = JSON.parse(document.getElementById('enterpriseData').textContent);
+const nameEnterprise = document.getElementById("nameEnterprise");
+const direccionEmpresa = document.getElementById("direccionEmpresa");
+const telefonoEmpresa = document.getElementById("telefonoEmpresa");
 
 userNameLoged.textContent = userData.nombre;
 userRucLoged.textContent = userData.ruc;
+
+nameEnterprise.textContent = enterpriseData[0].nombreempresa;
+direccionEmpresa.textContent = enterpriseData[0].direccion1;
+telefonoEmpresa.textContent = enterpriseData[0].telefono1;
+
+console.log(enterpriseData[0])
